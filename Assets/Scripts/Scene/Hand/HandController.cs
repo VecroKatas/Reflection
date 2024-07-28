a03d2c08dafb9aec0979a5efd487a0f38f51d731
VecroKatas <tymur.katas@gmail.com>
2024-07-28 03:25:24 +0300

    Started Hand

---
+++ Assets/Scripts/Scene/Hand/HandController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandController : MonoBehaviour
+{
+    public List<ICard> Cards = new List<ICard>();
+
+    public void DrawCards(int amount)
+    {
+        /*for (int i = 0; i < amount; i++)
+        {
+            DeckController.DrawCard();
+        }
+        
+        CreateCardInHand(); //HandCard
+        */
+    }
+
+    /*public void PlayCard(HandCard handCard)
+    {
+        TableController.PLayCard(handCard.ICard);
+
+        DeleteHandCard();
+    }*/
+    
+    
+}
