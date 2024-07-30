b2632b30808ff63c17b5957461b943322b7fc9fe
cenjoyer13 <lysenkosun20@gmail.com>
2024-07-30 14:07:03 +0300

    nothin

--- Assets/Scripts/Cards/OpportunityCard.cs
+++ Assets/Scripts/Cards/OpportunityCard.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class OpportunityCard : Card
 {
     [SerializeField] int[] _StatusStatModifiers; //how card affects status
+    [SerializeField] int[] _FunValue; //Value, that describes how fun it would be for shadow to take the opportunity
     public int[] StatusStatModifiers { get; }
 }
 
